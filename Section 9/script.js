de2c0d418dbaecec8498b1525a3d1249e45ee859
thesongmartins <martinsjehoshaphat@gmail.com>
2024-11-16 15:31:06 +0100

    string methods

--- Section 9/script.js
+++ Section 9/script.js
@@ -826,14 +826,12 @@ const gameEvents = new Map([
 ]);
 // 1.
 const events = [...new Set(gameEvents.values())];
-
 console.log(events);
 
 //2.
 gameEvents.delete(64);
 
 //3.
-
 const timee = [...gameEvents.keys()].pop();
 console.log(timee)
 console.log(`An event happened, on
@@ -844,6 +842,191 @@ for (const [min, event] of gameEvents) {
   const half = min <= 45 ? 'FIRST' : 'SECOND';
   console.log(`[${half} HALf] ${min}: ${event}`);
 }
+//Working with strings: Part 1
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('8737'[0]);
+
+console.log(airline.length);
+console.log('8737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+// Slice Method
+console.log(airline.slice(4));
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+
+const checkMidddleSeat = (seat) => {
+  //B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('You got the middle seat')
+  } else {
+    console.log('You got lucky');
+  }
+}
+checkMidddleSeat('118');
+checkMidddleSeat('23C');
+checkMidddleSeat('3E');
+
+// Reason why methods works on strings.
+console.log(new String('Jonas'));
+console.log(typeof new String('Jonas'));
+console.log(typeof new String('Jonas').slice(1));
+
+//Working with strings: Part 2
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// const passenger = 'jOnAs';
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
+// console.log(passengerLower);
+
+function UpperCase() {
+  const passenger = 'jOnAs';
+  const passengerLower = passenger.toLowerCase();
+  const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+  console.log(passengerLower);
+  console.log(passengerCorrect);
+}
+UpperCase();
+
+//Check User Input Emailing (Comparing Emails)
+const email = 'hello@jonas.io';
+const loginEmail = ' Hello@Jonas.Io \n';
+
+const lowerEmail = loginEmail.toLowerCase().trim();
+console.log(lowerEmail);
+console.log(email === lowerEmail);
+
+// replace() method
+const priceGB = '£288,97';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement = 'All passengers should come to the boarding door 23. Boarding door 23';
+console.log(announcement.replaceAll('door', 'gate'));
+
+// Booleans
+const planee = 'Airbus A320neo';
+console.log(planee.includes('A320'));
+console.log(planee.includes('Boeing'));
+console.log(planee.startsWith('Airb'));
+// console.log(planee.includes('A320'));
+if (planee.startsWith('Airbus') && planee.endsWith('neo')) {
+  console.log('Part of the New Airbus Family');
+};
+
+//Practice Exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log("You're NOT allowed on board");
+  } else {
+    console.log('Welcome On Board!');
+  }
+};
+checkBaggage('I have a laptop, some food and a pocket Knife');
+checkBaggage('Socka and a camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+//Working with strings: Part 3 split() and join()
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas schmedtmann'.split(' ');
+console.log(firstName, lastName);
+
+const newName = ['Mr', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = (name) => {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+}
+capitalizeName('jessica ann smith davis');
+capitalizeName('jonas schmedtmann');
+//padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+
+console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = (number) => {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+}
+console.log(maskCreditCard(83984975));
+console.log(maskCreditCard(988273864686842));
+console.log(maskCreditCard('7483475626983479837498565732'));
+
+// Repeat() method
+const message2 = 'Bad weather... All departures delayed...';
+console.log(message2.repeat(5));
+
+const planesInLine = (n) => {
+  console.log(`There are ${n} planes in line ${"✈️".repeat(n)}`);
+}
+planesInLine(5)
+planesInLine(3)
+planesInLine(13)
+
+//Challenge
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+})
+
+//Strings Practice
+// // Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Solution
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const outputt = `${type.startsWith('_Delayed') ? '🛑' : '  '}${type.replaceAll('_', '')}
+  ${getCode(from)} ${getCode(to)} (${time.replace(':', ('h'))})`.padStart(36);
+  console.log(outputt);
+}
+
+
+
+
+
 
 
 //CodeWars solution
@@ -852,10 +1035,13 @@ function highAndLow(numbers) {
   const number = numbers.split(" ");
 
   const intNumber = number.map(Number);
+  console.log(intNumber);
 
   const highest = Math.max(...intNumber);
+  console.log(highest);
   const lowest = Math.min(...intNumber);
-  return `${highest} ${lowest}`
+  console.log(lowest);
+  return `${highest} ${lowest} `
 }
 
 console.log(highAndLow("1 2 3 4 5"));
